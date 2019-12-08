3cff79e89e93674f18897809a85c1a8bb1e3745a
Daniel Reynolds <daniel.reynolds7a@gmail.com>
2019-12-08 18:25:48 +0000

    init repo

---
+++ script.js
@@ -0,0 +1,34 @@
+
+
+$('.autoplay').slick({
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    
+  ]
+});
+
+$('.single-item').slick();
